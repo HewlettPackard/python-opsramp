3d2b318d26f6f8653110c1f00dc6a41507194557
Domhnall Walsh <domhnall.eam.walsh@hpe.com>
2020-01-03 16:54:48 +0000

    Move entire licence and comments to top of file

--- opsramp/base.py
+++ opsramp/base.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # (c) Copyright 2019 Hewlett Packard Enterprise Development LP
 #
-# A minimal Python language binding for the OpsRamp REST API.
-#
-# base.py
-# Containing various base classes used in other parts of the library
-# but not intended for direct use by callers.
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -18,6 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# A minimal Python language binding for the OpsRamp REST API.
+#
+# base.py
+# Containing various base classes used in other parts of the library
+# but not intended for direct use by callers.
+#
+
 
 from __future__ import print_function
 from urllib import parse
