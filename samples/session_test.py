69d2781a06aed9727fdbb45527777d7ec33121f2
Josie Fegan <josie.fegan@hpe.com>
2020-12-08 13:34:45 +0000

    Sample to demo the "sessions" property
    This is just a clone of timezone_list.py with a custom session
    to illustrate the syntax for that.

---
+++ samples/session_test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# (c) Copyright 2020 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+import os
+import json
+import requests
+
+import opsramp.binding
+
+
+def connect():
+    url = os.environ['OPSRAMP_URL']
+    key = os.environ['OPSRAMP_KEY']
+    secret = os.environ['OPSRAMP_SECRET']
+    return opsramp.binding.connect(url, key, secret)
+
+
+def main():
+    ormp = connect()
+
+    # use a custom session with default parameters.
+    my_session = requests.session()
+    ormp.session = my_session
+
+    # simple demo that the custom session works.
+    assert ormp.session is my_session
+    global_cfg = ormp.config()
+    tzs = global_cfg.get_timezones()
+    print('[')
+    for t in tzs:
+        print('  ', json.dumps(t))
+    print(']')
+    # check that the custom session is still in use.
+    assert ormp.session is my_session
+
+
+if __name__ == "__main__":
+    main()
