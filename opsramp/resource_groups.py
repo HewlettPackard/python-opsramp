9a230cf85cccb4bdd9bf566c812db1fa71829f56
Mark Rawlings <Mark.Rawlings@hpe.com>
2022-08-11 11:56:22 +0100

    Fix resource group update operation (#132)
    * Fix resource group update operation
    The resource 'update' operation follows the same pattern as RBA update.
    The `id` of the resource to update is embedded in the JSON, not appended to the URL.
    
    * Realisation that resource groups are created/updated as lists of resources.
    These need to be fully populated to work correctly, no run-time fix-up of the JSON definition
    
    * Reformat with black
    
    * Remove uuid attribute from update operation completely.

--- opsramp/resource_groups.py
+++ opsramp/resource_groups.py
@@ -35,9 +35,9 @@ class ResourceGroups(ORapi):
         url_suffix = ""
         return self.api.post(url_suffix, json=definition)
 
-    def update(self, uuid, definition):
-        url_suffix = uuid
-        return self.api.post(url_suffix, json=definition)
+    def update(self, definition):
+        # UUID(s) specified in the request body(ies).
+        return self.api.post(json=definition)
 
     def delete(self, uuid):
         url_suffix = uuid
